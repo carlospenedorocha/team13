82b33781c3f00fe531e8673e2eb593deadf7d161
Ross Graeber <RGraeber@slb.com>
2016-12-07 14:35:16 -0600

    Correct dictionary issues

--- Capitals.py
+++ Capitals.py
@@ -16,9 +16,8 @@ class Capitals(object):
         entity['id'] = data['id']
         entity['country'] = data['country']
         entity['name'] = data['name']
-        entity['location'] = { 'latitude': data['location']['latitude'],
-                                'longitude': data['location']['longitude'],
-                            }
+        entity['location']['latitude'] = data['location']['latitude']
+        entity['location']['longitude'] = data['location']['longitude']
         entity['countryCode'] = data['countryCode']
         entity['continent'] = data['continent']
         return self.ds.put(entity)
