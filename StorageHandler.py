08b27a07133765e224042946761b936a4039c42c
Rachana Palacharla <RPalacharla@slb.com>
2016-12-08 13:37:34 -0600

    Fis storage handler errors

--- StorageHandler.py
+++ StorageHandler.py
@@ -1,7 +1,9 @@
 import utility
-# import cloudstorage
+import json
+#import cloudstorage
+
 from google.cloud import storage, exceptions
-from google.cloud.storage import Blob
+# from google.cloud.storage import Blob
 
 class StorageHandler:
     """Represent a google cloud storage handler"""
@@ -33,7 +35,7 @@ class StorageHandler:
         if bucket_exists is not None and not bucket_exists:
             self.create_bucket(bucket_name)
 
-        self.store_file_to_gcs(bucket_name, str(capital_id) + ".json")
+        self.store_file_to_gcs(bucket_name, str(capital_id) + ".json", item)
         return True
 
     def create_bucket(self, bucket_name):
@@ -46,34 +48,22 @@ class StorageHandler:
             print e
             return False
 
-    def store_file_to_gcs(self, bucket_name, filename):
+    def store_file_to_gcs(self, bucket_name, filename, item):
 
         if self.check_bucket(bucket_name):
-            bucket = self.gcs.get_bucket(bucket_name)
-            blob = Blob(filename, bucket)
-
+            # bucket = self.gcs.get_bucket(bucket_name)
+            filepath = "/" + bucket_name + "/" + filename
+            print 'Got bucket' + str(filepath)
+            with self.gcs.open(filepath, 'w', encoding='utf-8') as outfile:
+                outfile.write(unicode(json.dumps(item, ensure_ascii=False)))
+            self.gcs.close(filepath)
+            print 'wrote json'
             try:
-                with open(filename, 'rb') as input_file:
-                    blob.upload_from_file(input_file)
+                self.gcs.open(filepath, 'r')
+                # blob.upload_from_file(input_file)
                 return True
             except IOError:
-                print 'Error: Cannot find the file {}'.format(filename)
-        return False
-
-    def fetch_object_from_gcs(self, bucket_name, fileobject):
-
-        if self.check_bucket(bucket_name):
-            bucket = self.gcs.get_bucket(bucket_name)
-            blob = Blob(fileobject, bucket)
-            outfile = fileobject + ".out"
-
-            try:
-                with open(outfile, 'wb') as output_file:
-                    blob.download_to_file(output_file)
-                return True
-            except IOError:
-                print ('Error: Cannot find the object {}'.format(fileobject))
-
+                print 'Error: Cannot open the file {}'.format(filepath)
         return False
 
     def list_objects(self, bucket_name):
