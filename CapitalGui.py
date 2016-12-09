be4475880a4ef989a7e78ce8fa2d01ac8664001f
Dave Barnett <barnett2@slb.com>
2016-12-09 09:48:34 -0600

    Store initial GUI components

---
+++ CapitalGui.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from google.cloud import datastore
+import utility
+import Capitals
+
+class CapitalGui:
+
+    def __init__(self):
+        self.ds = datastore.Client(utility.project_id())
+        self.kind = "capitals"
+
+    def store_greeting(self, comment):
+        key = self.ds.key(self.kind)
+        entity = datastore.Entity(key)
+
+        entity['comment'] = comment
+        entity['timestamp'] = datetime.utcnow()
+
+        return self.ds.put(entity)
+
+    def fetch_capitals(self):
+        capital = Capitals.Capitals()
+        return capital.fetch_capitals()
+
+    def get_query_results(self, query):
+        results = list()
+        for entity in list(query.fetch()):
+            results.append(dict(entity))
+        return results
+
